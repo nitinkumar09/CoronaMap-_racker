18716175a6a7ebfed0a9a01e76959b072f4c17b6
Nitin kumar <149141072+nitinkumar09@users.noreply.github.com>
2025-05-18 13:13:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+function updateMap() {
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(res => {
+            console.log(res.data);
+            res.data.forEach(element => {
+                longitude = element.longitude
+                latitude = element.latitude
+                statename = element.name
+                //console.log(longitude);
+                // console.log(latitude)
+                cases = element.infected;
+                console.log(cases)
+                if (cases > 255) {
+                    color = "rgb(255,0 ,0)";
+                }
+                else {
+                    color = `rgb(${cases},0 ,0)`
+                }
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+
+            });
+
+
+        });
+
+
+}
+console.log("Updating map with realtime data");
+updateMap();
+// let interval = 2000;
+// setInterval(updateMap , interval);
+
+
+
